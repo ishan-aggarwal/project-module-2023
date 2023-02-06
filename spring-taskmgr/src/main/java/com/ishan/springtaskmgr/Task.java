b8d56fa98e5548c6cc4103f677880bd8b5801a78
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2023-02-06 22:25:39 +0530

    Spring boot basic task manager application

---
+++ spring-taskmgr/src/main/java/com/ishan/springtaskmgr/Task.java
@@ -0,0 +1,50 @@
+package com.ishan.springtaskmgr;
+
+import java.util.Date;
+
+public class Task {
+   private int id;
+   private String title;
+   private String description;
+   private Date dueDate;
+
+    public Task(int id, String title, String description, Date dueDate) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+}
+
