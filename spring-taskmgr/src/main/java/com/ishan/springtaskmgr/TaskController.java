8ccec6064c5299ecf0842ee31e2cc968a3707f7b
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2023-02-06 23:12:22 +0530

    Spring boot basic task manager application added additional end points

--- spring-taskmgr/src/main/java/com/ishan/springtaskmgr/TaskController.java
+++ spring-taskmgr/src/main/java/com/ishan/springtaskmgr/TaskController.java
@@ -1,14 +1,14 @@
 package com.ishan.springtaskmgr;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Predicate;
 
 @RestController
 public class TaskController {
@@ -23,15 +23,90 @@ public class TaskController {
         taskList.add(new Task(taskId.incrementAndGet(), "Task 3", "Description 3", new Date()));
     }
 
+    /**
+     * Show all existing tasks
+     * GET /tasks
+     *
+     * @return List of tasks
+     */
     @GetMapping("/tasks")
     private List<Task> getTasks() {
         return taskList;
     }
 
+    /**
+     * Create a new task
+     * POST /tasks
+     * Body:
+     * <pre>
+     *      {
+     *          "title": "Task 4",
+     *          "description": "Description 4",
+     *          "dueDate": "2021-01-01"
+     *      }
+     *  </pre>
+     *
+     * @param task Task object sent by client
+     * @return Task object created
+     */
     @PostMapping("/tasks")
     Task createTask(@RequestBody Task task) {
         var newTask = new Task(taskId.incrementAndGet(), task.getTitle(), task.getDescription(), task.getDueDate());
         this.taskList.add(newTask);
         return newTask;
+    }
+
+    /**
+     * Get a task by id
+     *
+     * @param id
+     * @return Task object
+     */
+    @GetMapping("/tasks/{id}")
+    Task getTask(@PathVariable("id") Integer id) {
+        return taskList.stream().filter(task -> task.getId() == id).findFirst().orElseThrow(ResourceNotFoundException::new);
+    }
+
+    /**
+     * Delete a task by given id
+     *
+     * @param id Task id to delete
+     * @return the deleted task
+     */
+    @DeleteMapping("/tasks/{id}")
+    Task deleteTask(@PathVariable("id") Integer id) {
+        Optional<Task> optionalTask = taskList.stream().filter(task -> task.getId() == id).findFirst();
+        if (optionalTask.isPresent()) {
+            taskList.remove(optionalTask.get());
+            return optionalTask.get();
+        }
+        throw new ResourceNotFoundException("Task with id: " + id + " not found");
+    }
+
+    /**
+     * Update a task by given id
+     *
+     * @param id   Task id to update
+     * @param task Task object sent by client
+     * @return the updated task
+     */
+    @PatchMapping("/tasks/{id}")
+    Task updateTask(@PathVariable("id") Integer id, @RequestBody Task task) {
+        Optional<Task> optionalTask = taskList.stream().filter(item -> item.getId() == id).findFirst();
+        if (optionalTask.isEmpty()) {
+            throw new ResourceNotFoundException("Task with id: " + id + " not found");
+        }
+
+        Task taskToUpdate = optionalTask.get();
+        if (task.getTitle() != null) {
+            taskToUpdate.setTitle(task.getTitle());
+        }
+        if (task.getDescription() != null) {
+            taskToUpdate.setDescription(task.getDescription());
+        }
+        if (task.getDueDate() != null) {
+            taskToUpdate.setDueDate(task.getDueDate());
+        }
+        return taskToUpdate;
     }
 }
