6a5cd2040a7184c20a56ba806d11154839e674f6
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2023-02-06 22:00:40 +0530

    spring boot basics project

---
+++ spring-basics/src/main/java/com/ishan/springbasics/HelloController.java
@@ -0,0 +1,15 @@
+package com.ishan.springbasics;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "<b>Hello World!</b>";
+    }
+}
+
+
