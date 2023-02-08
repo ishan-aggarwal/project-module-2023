1237785cf90ef3e3070c4b2a07615f3489a7adf4
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2023-02-08 22:47:24 +0530

    Changes for separate packages and adding HTTP response codes as well

---
+++ spring-taskmgr/src/main/java/com/ishan/springtaskmgr/entities/Task.java
@@ -0,0 +1,50 @@
+package com.ishan.springtaskmgr.entities;
+
+import java.util.Date;
+
+public class Task {
+    Integer id;
+    String title;
+    String description;
+    String dueDate;
+
+    public Task(Integer id, String title, String description, String dueDate) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+}
+
